ddf26f67a1142a9ba0599f898b9fa613d0915136
AdriARE <adrian.r.estevez@gmail.com>
2025-10-03 19:09:28 +0000

    Hasta el paso 5

--- src/app.py
+++ src/app.py
@@ -4,9 +4,17 @@ import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 
 # load the .env file variables
-load_dotenv()
+load_dotenv(override=True)
 
 
 # Spotify API credentials
 client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
+print("✅ Conexión con Spotify establecida")
